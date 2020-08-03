62da29f553d4ea87b2f152968f754ecc958b8a77
MichaelAshmead1 <michael@dcdigitalapps.com>
2020-08-03 14:07:47 -0400

    Add environment variables to prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   auth0_domain: 'greengrowthcpas.us.auth0.com',
-  auth0_client_id: 'ITcab3ln4IopywbhJZ1A1bcqhDwcST7u'
+  auth0_client_id: 'ITcab3ln4IopywbhJZ1A1bcqhDwcST7u',
+  auth0_connection: 'Username-Password-Authentication',
+  api_url: 'http://localhost:3000/api'
 };
