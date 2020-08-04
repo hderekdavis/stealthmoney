7053c2664aff4e9c5385655f8e43ea3c1e6d0290
MichaelAshmead1 <michael@dcdigitalapps.com>
2020-08-03 21:01:19 -0400

    Changing api_url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   auth0_domain: 'greengrowthcpas.us.auth0.com',
   auth0_client_id: 'ITcab3ln4IopywbhJZ1A1bcqhDwcST7u',
   auth0_connection: 'Username-Password-Authentication',
-  api_url: 'http://localhost:3000/api'
+  api_url: 'https://clearviewmoney.com/api'
 };
